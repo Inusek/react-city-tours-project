9817e08fc64ac708e02c64b407e1ec0cfd7edc17
unknown <arkadiusz.micek@gmail.com>
2020-02-19 08:41:34 +0100

    First Commit

---
+++ src/components/TourList/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Tour from "../Tour";
+import "./tourlist.scss";
+import { tourData } from "../../tourData";
+
+export default class TourList extends Component {
+    state = {
+        tours: tourData
+    };
+    removeTour = id => {
+        const { tours } = this.state;
+        const sortedTOur = tours.filter(tour => tour.id !== id);
+        this.setState({
+            tours: sortedTOur
+        });
+    };
+    render() {
+        // console.log(this.state.tours);
+        const { tours } = this.state;
+        return (
+            <section className="tourList">
+                {tours.map(tour => (
+                    <Tour
+                        key={tour.id}
+                        tour={tour}
+                        removeTour={this.removeTour}
+                    />
+                ))}
+            </section>
+        );
+    }
+}
